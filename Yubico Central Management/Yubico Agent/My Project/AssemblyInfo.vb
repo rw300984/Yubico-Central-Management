8592872f8d6027ef651847c6e88412986cf9e267
Ronny Wolf <rw300984@hotmail.com>
2018-01-11 14:00:04 +0100

    Changed to WinForms

--- Yubico Central Management/Yubico Agent/My Project/AssemblyInfo.vb
+++ Yubico Central Management/Yubico Agent/My Project/AssemblyInfo.vb
@@ -1,9 +1,6 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
-Imports System.Globalization
-Imports System.Resources
-Imports System.Windows
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -11,38 +8,17 @@ Imports System.Windows
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("Yubico_Agent")>
+<Assembly: AssemblyTitle("Yubico Agent")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("Yubico_Agent")>
+<Assembly: AssemblyProduct("Yubico Agent")>
 <Assembly: AssemblyCopyright("Copyright ©  2018")>
 <Assembly: AssemblyTrademark("")>
-<Assembly: ComVisible(false)>
 
-'In order to begin building localizable applications, set 
-'<UICulture>CultureYouAreCodingWith</UICulture> in your .vbproj file
-'inside a <PropertyGroup>.  For example, if you are using US english 
-'in your source files, set the <UICulture> to "en-US".  Then uncomment the
-'NeutralResourceLanguage attribute below.  Update the "en-US" in the line
-'below to match the UICulture setting in the project file.
-
-'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)> 
-
-
-'The ThemeInfo attribute describes where any theme specific and generic resource dictionaries can be found.
-'1st parameter: where theme specific resource dictionaries are located
-'(used if a resource is not found in the page, 
-' or application resource dictionaries)
-
-'2nd parameter: where the generic resource dictionary is located
-'(used if a resource is not found in the page, 
-'app, and any theme specific resource dictionaries)
-<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
-
-
+<Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("0a8f90ed-da62-4991-a2fc-958893ac11c5")>
+<Assembly: Guid("06f80af1-b599-4fa2-b6df-90222fad6f87")>
 
 ' Version information for an assembly consists of the following four values:
 '
