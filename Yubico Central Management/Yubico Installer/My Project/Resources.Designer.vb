1ec1e5cd3bb98f86c85b6c3ad7175bcc85938870
Ronny Wolf <rw300984@hotmail.com>
2018-01-21 05:49:36 +0100

    Icon added

--- Yubico Central Management/Yubico Installer/My Project/Resources.Designer.vb
+++ Yubico Central Management/Yubico Installer/My Project/Resources.Designer.vb
@@ -79,5 +79,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        '''</summary>
+        Friend ReadOnly Property Yubico_icon() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Yubico_icon", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
+            End Get
+        End Property
     End Module
 End Namespace
