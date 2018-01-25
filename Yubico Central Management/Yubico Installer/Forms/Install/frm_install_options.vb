bd78178df44d3225e8ec4999b3401fa9d5883f59
Ronny Wolf <rw300984@hotmail.com>
2018-01-25 12:09:49 +0100

    Bugfix on Yubico Installer

--- Yubico Central Management/Yubico Installer/Forms/Install/frm_install_options.vb
+++ Yubico Central Management/Yubico Installer/Forms/Install/frm_install_options.vb
@@ -5,6 +5,7 @@
     Dim config_textbox3 As Integer = 0
     Dim config_textbox6 As Integer = 0
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        cfg.install_options_success = 0
         If RadioButton2.Checked Then
             RadioButton1.Checked = False
             GroupBox1.Enabled = False
@@ -36,6 +37,7 @@
     End Sub
 
     Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        cfg.install_options_success = 0
         If RadioButton1.Checked Then
             RadioButton2.Checked = False
             GroupBox2.Enabled = False
@@ -58,7 +60,7 @@
             If TextBox6.Text <> "" Then
                 config_textbox6 = 1
             End If
-            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 3 Then
+            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 4 Then
                 cfg.install_options_success = 1
             Else
                 cfg.install_options_success = 0
@@ -73,23 +75,28 @@
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        config_combobox = 0
         cfg.install_options_business_auth = ComboBox1.Text
         If ComboBox1.Text = "" Then
+            config_combobox = 0
             cfg.install_options_success = 0
         Else
             If ComboBox1.Text = "Integrated" Then
-
                 TextBox2.Enabled = False
                 TextBox3.Enabled = False
-                TextBox6.Enabled = False
-
-                cfg.install_options_success = 1
+                TextBox6.Enabled = True
+                config_combobox = 1
+                If config_combobox + config_textbox6 = 2 Then
+                    cfg.install_options_success = 1
+                Else
+                    cfg.install_options_success = 0
+                End If
             Else
                 TextBox2.Enabled = True
                 TextBox3.Enabled = True
                 TextBox6.Enabled = True
                 config_combobox = 1
-                If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 3 Then
+                If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 4 Then
                     cfg.install_options_success = 1
                 Else
                     cfg.install_options_success = 0
@@ -99,12 +106,14 @@
     End Sub
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        config_textbox2 = 0
         cfg.install_options_business_username = TextBox2.Text
         If TextBox2.Text = "" Then
+            config_textbox2 = 0
             cfg.install_options_success = 0
         Else
             config_textbox2 = 1
-            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 3 Then
+            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 4 Then
                 cfg.install_options_success = 1
             Else
                 cfg.install_options_success = 0
@@ -113,12 +122,14 @@
     End Sub
 
     Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        config_textbox3 = 0
         cfg.install_options_business_password = TextBox3.Text
         If TextBox3.Text = "" Then
+            config_textbox3 = 0
             cfg.install_options_success = 0
         Else
             config_textbox3 = 1
-            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 3 Then
+            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 4 Then
                 cfg.install_options_success = 1
             Else
                 cfg.install_options_success = 0
@@ -127,16 +138,54 @@
     End Sub
 
     Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+        config_textbox6 = 0
         cfg.install_options_business_server = TextBox6.Text
         If TextBox6.Text = "" Then
+            config_textbox6 = 0
             cfg.install_options_success = 0
         Else
             config_textbox6 = 1
-            If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 3 Then
-                cfg.install_options_success = 1
+            Select Case cfg.install_options_business_auth
+                Case "Integrated"
+                    If config_combobox + config_textbox6 = 2 Then
+                        cfg.install_options_success = 1
+                    Else
+                        cfg.install_options_success = 0
+                    End If
+                Case "Basic"
+                    If config_combobox + config_textbox2 + config_textbox3 + config_textbox6 = 4 Then
+                        cfg.install_options_success = 1
+                    Else
+                        cfg.install_options_success = 0
+                    End If
+            End Select
+
+        End If
+    End Sub
+
+    Private Sub frm_install_options_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
+        If Visible Then
+            If ComboBox1.Text <> "" Then
+                config_combobox = 1
             Else
-                cfg.install_options_success = 0
+                config_combobox = 0
             End If
+            If TextBox2.Text <> "" Then
+                config_textbox2 = 1
+            Else
+                config_textbox2 = 0
+            End If
+            If TextBox3.Text <> "" Then
+                config_textbox3 = 1
+            Else
+                config_textbox3 = 0
+            End If
+            If TextBox6.Text <> "" Then
+                config_textbox6 = 1
+            Else
+                config_textbox6 = 0
+            End If
+
         End If
     End Sub
 End Class
